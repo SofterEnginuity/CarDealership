ca5b383965cb192827ec4a9d5fdf48bc918ab209
Marqae Morris <mamorris920@gmail.com>
2025-05-12 16:02:37 -0400

    initialized methods for specific user requests

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,8 +1,39 @@
 package com.pluralsight;
 
 public class UserInterface {
+    public void display(){
+        System.out.println("Complete");
+    }
+    public void processGetByPriceRequest(){
 
+    }
+    public void processGetByMakeModelRequest(){
 
+    }
+    public void processGetByYearRequest(){
 
+    }
+    public void processGetByColorRequest(){
 
+    }
+    public void processGetByMileageRequest(){
+
+    }public void processGetByVehicleTypeRequest(){
+
+    }
+    public void processGetAllVehiclesRequest(){
+        System.out.println("Complete");
+    }
+    public void processAddVehicleRequest(){
+    }
+    public void processRemoveVehicleRequest(){
+
+    }
+
+    private void init(){
+        System.out.println("Complete");
+    }
+    public void displayVehicles(){
+        System.out.println("Complete");
+    }
 }
