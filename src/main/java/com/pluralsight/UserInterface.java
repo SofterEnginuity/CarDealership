2398f6f3d9c06ecaf68b1a5a5c3d0955f66d43b2
Marqae Morris <mamorris920@gmail.com>
2025-05-12 22:34:20 -0400

    created switch case for dispaly menu

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,14 +1,70 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
     public void display(){
-        System.out.println("Complete");
+        Dealership dealership;
+        init();
+ //what???
+        Scanner scanner = new Scanner("inventory.csv");
+        System.out.println("Calling init");
+         init();
+
+        System.out.println("Please make a selection: ");
+        int selection = scanner.nextInt();
+
+        System.out.println("1 - Display All Vehicles");
+        System.out.println("2 - Filter by Price");
+        System.out.println("3 - Filter by Make and model");
+        System.out.println("4 - Filter by Year");
+        System.out.println("5 - Filter by Mileage");
+        System.out.println("6 - Filter by Vehicle Type");
+        System.out.println("7 - Add a Vehicle");
+        System.out.println("8 - Remove a Vehicle");
+
+        switch (selection) {
+            case 1:
+                display();
+                System.out.println("all");
+                break;
+            case 2:
+                processGetByPriceRequest();
+                break;
+//            case 3:
+//                processGetByMakeModelRequest();
+//                break;
+//            case 4:
+//                processGetByYearRequest();
+//                break;
+//            case 5:
+//                processGetByColorRequest();
+//                break;
+//            case 6:
+//                processGetByMileageRequest();
+//                break;
+//            case 7:
+//                processGetByVehicleTypeRequest();
+//                break;
+//            case 8:
+//                processAddVehicleRequest();
+//                break;
+//            case 9:
+//                processRemoveVehicleRequest();
+//                break;
+            default:
+                System.out.println("Please make a valid selection.");
+                break;
+        }
+
+
+
     }
     public void processGetByPriceRequest(){
-
+        System.out.println("Price");
     }
     public void processGetByMakeModelRequest(){
-
+        System.out.println("Make / Model");
     }
     public void processGetByYearRequest(){
 
